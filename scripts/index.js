72ff03b74f1a43f692438958ac5282534546daa8
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-01-27 23:46:42 +0300

    добавлено меню

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,15 @@ const editBtn = document.querySelector('.profile__edit-button');
 const closeBtn = document.querySelector('.modal__close-icon');
 const profileEditBlock = document.querySelector('.modal__container');
 const profileBanner = document.querySelector('.profile__banner');
+
+const formElem = document.querySelector('.modal__form_place_regform');
+const username = document.querySelector('.profile__name');
+const userjob = document.querySelector('.profile__occupation');
+const userphoto = document.querySelector('.profile__foto');
+const nameInput = document.querySelector('.modal__field_type_name');
+const jobInput = document.querySelector('.modal__field_type_occupation');
+const fotoInput = document.querySelector('.modal__field_type_foto-link');
+
 function visibleBlock(div) {
   div.classList.add('container_visibility');
 }
@@ -9,7 +18,26 @@ function hideBlock(div) {
   div.classList.remove('container_visibility');
 }
 
-editBtn.addEventListener('click', () => visibleBlock(profileEditBlock));
+function visibleProfileBlock() {
+  nameInput.value = username.textContent;
+  jobInput.value = userjob.textContent;
+  visibleBlock(profileEditBlock);
+}
+
+function submitFormHandler(evt) {
+  evt.preventDefault();
+
+  username.textContent = nameInput.value;
+  userphoto.src = fotoInput.value;
+  userjob.textContent = jobInput.value;
+  hideBlock(profileEditBlock);
+}
+
+
+
+editBtn.addEventListener('click', () => visibleProfileBlock());
 editBtn.addEventListener('click', () => visibleBlock(profileBanner));
+
 closeBtn.addEventListener('click', () => hideBlock(profileEditBlock));
 closeBtn.addEventListener('click', () => hideBlock(profileBanner));
+formElem.addEventListener('submit', submitFormHandler); 
