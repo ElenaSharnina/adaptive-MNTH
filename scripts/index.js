c40d8d84a2b6d3a865a191c46e0eab74213740a0
ElenaSharnina <92175599+ElenaSharnina@users.noreply.github.com>
2022-01-29 00:25:47 +0300

    накидала галерею

--- scripts/index.js
+++ scripts/index.js
@@ -31,13 +31,83 @@ function submitFormHandler(evt) {
   userphoto.src = fotoInput.value;
   userjob.textContent = jobInput.value;
   hideBlock(profileEditBlock);
+  hideBlock(profileBanner);
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(item => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault();
 
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
 
 editBtn.addEventListener('click', () => visibleProfileBlock());
 editBtn.addEventListener('click', () => visibleBlock(profileBanner));
 
 closeBtn.addEventListener('click', () => hideBlock(profileEditBlock));
 closeBtn.addEventListener('click', () => hideBlock(profileBanner));
-formElem.addEventListener('submit', submitFormHandler); 
+formElem.addEventListener('submit', submitFormHandler);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+    note: 'собственно любой текст'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+    note: 'собственно любой текст'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+    note: 'собственно любой текст'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+    note: 'собственно любой текст'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+    note: 'собственно любой текст'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+    note: 'собственно любой текст'
+  }
+];
+const picContainer = document.querySelector('.element_litle-size');
+const picTemplate = document.querySelector('#element').content;
+
+function addCard(item) {
+  picContainer.prepend(item);
+}
+function createCard(item) {
+  const picElement = picTemplate.cloneNode(true);
+  const image = picElement.querySelector('.element__image');
+  picElement.querySelector('.element__name').textContent = item.name;
+  image.src = item.link;
+  image.alt = item.name;
+
+  /*image.addEventListener('click', () => { //открытие модального окна у картинки
+    modalLabel.textContent = item.name;
+    modalImage.src = item.link;
+    modalImage.alt = item.name;
+    openModal(modalCard);
+  })*/
+
+  return picElement;
+}
+function renderCards(array) { //отбираем элементы из массива
+  array.forEach((item) =>
+    addCard(createCard(item)));
+}
+renderCards(initialCards);
